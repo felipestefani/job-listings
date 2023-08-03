c34301dbe91dca3b3ec50912a43d65f8e05abcba
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-02 23:51:52 -0300

    fix: footer author

--- src/App.vue
+++ src/App.vue
@@ -66,7 +66,7 @@
         </main>
         <footer class="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+            Coded by <a href="https://www.linkedin.com/in/felipe-stefani-a35185116/" target="_blank">Felipe Stefani</a>.
         </footer>
     </div>
 </template>
