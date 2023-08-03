b1ad07a278ba8b7ff89ce334160ef7e53eacb5b0
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-03 00:38:52 -0300

    fix: config deploy

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     // const getImageUrl = filename =>  import.meta.env.BASE_URL + 'src/assets/' + filename
     const getImageUrl = filename =>  {
         const baseUrl = import.meta.env.PROD ? '/job-listings/' : '/'
-        return baseUrl + 'src/' + filename
+        return baseUrl + 'src/assets/' + filename
     }
     const addFilter = job_filter => !filter.value.includes(job_filter) ? filter.value = [...filter.value, job_filter] : null
     const removeFilter = job_filter_remove => filter.value = filter.value.filter(item => item!==job_filter_remove)
